b7d63406c28ade65fcdf5c687d17a2565d609620
Rafik ElAakil <rafik.elaakil@devoteam.lu>
2018-09-21 17:09:32 +0200

    Initial Checkins

---
+++ src/webparts/crudProject/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICrudProjectWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CrudProjectWebPartStrings' {
+  const strings: ICrudProjectWebPartStrings;
+  export = strings;
+}
